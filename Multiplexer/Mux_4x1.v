ccba9dd4dceb1049f7194c0d4be5fbf5e2809543
Manjunath Kalmath <48850794+ManjunathKalmath@users.noreply.github.com>
2020-11-06 14:33:48 +0530

    Update Mux_4x1.v

--- Multiplexer/Mux_4x1.v
+++ Multiplexer/Mux_4x1.v
@@ -1,3 +1,15 @@
 module mux_4x1();
-input 
+  input [3:0] a,b,c,d;
+  input [2:0] selcet_line;
+  output reg [4:0] result;
+  
+  always @(*)
+    begin
+      case(select_line)
+        2'b00 : result <= a;
+        2'b01 : result <= b;
+        2'b10 : result <= c;
+        2'b11 : result <= d;
+      endcase
+    end
 endmodule
